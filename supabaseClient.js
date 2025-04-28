6b2d215f248850cab1933f782d04e792cd6dacde
badoettoea <87426546+Badoettoea@users.noreply.github.com>
2025-04-28 19:05:15 +0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 // supabaseClient.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-export const supabaseUrl = 'https://your-project.supabase.co'   // GANTI
-export const supabaseKey = 'public-anon-key'                    // GANTI
+export const supabaseUrl = 'https://fcrzfzpfzuqqcsivwmyo.supabase.co'   // GANTI
+export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjcnpmenBmenVxcWNzaXZ3bXlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNzg5NjQsImV4cCI6MjA2MDk1NDk2NH0.VJ4vYUdcDzpLhyyFFscuM9yzNB8eVP0nEl7-ByJJJHc'                    // GANTI
 export const supabase = createClient(supabaseUrl, supabaseKey)
