df17ebe5a10f8740a3b5016f0cbe1b36467c52fc
badoettoea <87426546+Badoettoea@users.noreply.github.com>
2025-04-28 22:48:23 +0700

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,1 +1,11 @@
+import { supabase } from './supabaseClient.js';
 
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (!error) {
+    window.location.href = '/login.html'; // Arahkan ke halaman login
+  } else {
+    console.error('Logout error:', error);
+    alert('Gagal logout. Silakan coba lagi.');
+  }
+}
